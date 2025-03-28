6cc48e4b807c7689f26dccb3e7643db2ec7d026f
sumit rai <RAIS74184@gmail.com>
2025-03-28 21:47:54 +0530

    binarySearchTree

---
+++ BinaySearchTree/Implentation.java
@@ -0,0 +1,23 @@
+package BinaySearchTree;
+
+import java.util.*;
+public class Implentation {
+  class Node{
+    int val;
+    Node left,right;
+    public Node(int val){
+      this.val=val;
+      this.left=null;
+      this.right=null;
+    }
+  }
+
+  public List<Integer>BinarySearchTree(Node root){
+    List<Integer>ans=new ArrayList<>();
+    return ans;
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+}
