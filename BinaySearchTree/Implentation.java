faf7d24994bd1cdb7d9a65fe5f3ca3ba0612dee6
sumit rai <RAIS74184@gmail.com>
2025-04-12 14:22:35 +0530

    bst

--- BinaySearchTree/Implentation.java
+++ BinaySearchTree/Implentation.java
@@ -1,23 +1,54 @@
 package BinaySearchTree;
 
-import java.util.*;
+//Inorder Traversal for the increasing order output
 public class Implentation {
-  class Node{
+  static class TreeNode{
     int val;
-    Node left,right;
-    public Node(int val){
+    TreeNode left,right;
+    public TreeNode(int val){
       this.val=val;
       this.left=null;
       this.right=null;
     }
   }
 
-  public List<Integer>BinarySearchTree(Node root){
-    List<Integer>ans=new ArrayList<>();
-    return ans;
+public static class BST{
+  public void inorderTraversal(TreeNode root){
+    if(root==null){
+      return;
+    }
+    inorderTraversal(root.left);
+    System.out.println(root.val+" ");
+    inorderTraversal(root.right);
   }
-  
+
+  public TreeNode constructBST(int arr[]){
+    TreeNode root=null;
+    for(int i=0;i<arr.length;i++){
+      root=insertIntoBST(root, arr[i]);
+    }
+    return root;
+  }
+
+   public TreeNode insertIntoBST(TreeNode root, int val) {
+    // List<Integer>ans=new ArrayList<>();
+        if (root == null) {
+           return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+}
   public static void main(String[] args) {
-    
+    int arr[]={9,8,4,6,11,15,7,19,10};
+    BST obj =new BST();
+    TreeNode root=obj.constructBST(arr);
+    obj.inorderTraversal(root);
+
   }
 }
--- OopsAdvanceConcept/MyGeneric.java
+++ OopsAdvanceConcept/MyGeneric.java
@@ -1,59 +1,59 @@
-package OopsAdvanceConcept;
+// package OopsAdvanceConcept;
 
-public class MyGeneric {
-  public static void main(String[] args) {
-    IntegerDisplayer obj=new IntegerDisplayer();
-    obj.setNum(23);
-    obj.display();
+// public class MyGeneric {
+//   public static void main(String[] args) {
+//     IntegerDisplayer obj=new IntegerDisplayer();
+//     obj.setNum(23);
+//     obj.display();
 
-    // Displayer<Integer>obj1=new Displayer<Integer>();
-    // obj1.setNum(234);
-    // obj1.display();
+//     // Displayer<Integer>obj1=new Displayer<Integer>();
+//     // obj1.setNum(234);
+//     // obj1.display();
 
 
-    Displayer<College>obj2=new Displayer<College>();
-    obj2.setNum(new College());
-    obj2.display();
+//     Displayer<College>obj2=new Displayer<College>();
+//     obj2.setNum(new College());
+//     obj2.display();
 
-  }
-}
-class IntegerDisplayer{
-  Integer num;
-  public void setNum(Integer num){
-    this.num=num;
-  }
-  public void display(){
-    System.out.println("element ->" + this.num);
-  }
-}
+//   }
+// }
+// class IntegerDisplayer{
+//   Integer num;
+//   public void setNum(Integer num){
+//     this.num=num;
+//   }
+//   public void display(){
+//     System.out.println("element ->" + this.num);
+//   }
+// }
 
-//now what if we want to change for the different variable in the problems
+// //now what if we want to change for the different variable in the problems
 
- class Displayer<T extends College>{//T is reference variable
-  T num;
-  public void setNum(T num){
-    this.num=num;
-  }
-  public void display(){
-    System.out.println("element ->" + this.num);
-  }
-}
+//  class Displayer<T extends College>{//T is reference variable
+//   T num;
+//   public void setNum(T num){
+//     this.num=num;
+//   }
+//   public void display(){
+//     System.out.println("element ->" + this.num);
+//   }
+// }
 
-class Student extends College{
-  void printName(){
-    System.out.println("name printed");
-  }
-}
+// class Student extends College{
+//   void printName(){
+//     System.out.println("name printed");
+//   }
+// }
 
-class Teacher extends College{
-  void printTeacherName(){
-    System.out.println("Teacher name is printed");
-  }
-}
+// class Teacher extends College{
+//   void printTeacherName(){
+//     System.out.println("Teacher name is printed");
+//   }
+// }
 
-class College{
-  void printCollegeName(){
-    System.out.println("IIT BOMBAY ");
-  }
-}
+// class College{
+//   void printCollegeName(){
+//     System.out.println("IIT BOMBAY ");
+//   }
+// }
 
