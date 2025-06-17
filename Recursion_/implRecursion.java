16d86ad8083a3120b3fe33e33f7a2e3a32eada6e
SUMIT <rais74184@gmail.com>
2025-06-17 19:19:11 +0530

    recursion

--- Recursion_/implRecursion.java
+++ Recursion_/implRecursion.java
@@ -25,15 +25,60 @@ public class implRecursion {
         System.out.println(res);
         return  res;
     }
+
+    public void ReverseNum(int n){
+        //divide any number from the %
+        if(n==0){
+            return;
+        }
+//        boolean flag=false;
+//        if(n<0){
+//            flag=true;
+//        }
+        System.out.print(n%10);
+        ReverseNum(n/10);
+    return;
+    }
+
+    //string normal technique using recursion
+    public String StringReverse(String Inp, int index, String rev){
+
+        if(index==Inp.length()){
+            return rev;
+        }
+        rev=Inp.charAt(index)+rev;
+//        rev=Inp[index]+rev;//this is for array traversing
+         return StringReverse(Inp,index+1,rev);
+
+    }
+
+    //using the recursion technique
+    public String StringRNew(String Inp,int index,String rev){
+    if(index==Inp.length()){
+        return rev;
+    }
+
+        rev=StringRNew(Inp,index+1,rev);//
+        rev=rev+Inp.charAt(index);
+        return rev;
+}
+
     public static void main(String[] args) {
         implRecursion imp=new implRecursion();
 
 //        imp.fun(5);
-       int res=  imp.fib(6);
-        System.out.println(res);
+//       int res=  imp.fib(6);
+//        System.out.println(res);
 //       int ans = imp.Nat(6);
+        imp.ReverseNum(543210);
+        System.out.println();
 
 
+       String res=imp.StringReverse("54321",0,"");
+        System.out.println(res);
+
+        String res1=imp.StringRNew("54321",0,"");
+        System.out.println(res1);
 
     }
 }
