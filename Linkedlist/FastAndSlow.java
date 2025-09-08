b35dca7544179a9e47cf4e134d3fa3dc4620b570
SUMIT <rais74184@gmail.com>
2025-09-08 12:45:58 +0530

    fast&slow

--- Linkedlist/FastAndSlow.java
+++ Linkedlist/FastAndSlow.java
@@ -1,5 +1,44 @@
 package Linkedlist;
 
-//important pattern
+class Node<T> {
+    T data;
+    @SuppressWarnings("rawtypes")
+    Node next;
+
+    Node() {
+        this.next = null;
+
+    }
+
+    Node(T data) {
+        this.next = null;
+        this.data = data;
+    }
+}
+// important pattern
 public class FastAndSlow {
+
+    Node<Integer> head;
+    int size;
+
+    FastAndSlow() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unused")
+    public Node middleNode(Node head){
+        Node fast=head;
+        Node slow=head;
+
+        //what if  there is only one node then 
+        //that's why && operator
+
+        while(fast.next==null && fast==null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+
 }
