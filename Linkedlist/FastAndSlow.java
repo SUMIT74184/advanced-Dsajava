7247866cb4a6c118a13ee7a4c6a5ce20ecb6ab8c
SUMIT <rais74184@gmail.com>
2025-09-12 19:03:59 +0530

    test

--- Linkedlist/FastAndSlow.java
+++ Linkedlist/FastAndSlow.java
@@ -1,24 +1,21 @@
 package Linkedlist;
 
-class Node<T> {
+class Node1<T> {
     T data;
-    @SuppressWarnings("rawtypes")
-    Node next;
+    Node1<T> next;          // use a typed next
 
-    Node() {
+    Node1() {
         this.next = null;
-
     }
 
-    Node(T data) {
-        this.next = null;
+    Node1(T data) {
         this.data = data;
+        this.next = null;
     }
 }
-// important pattern
-public class FastAndSlow {
 
-    Node<Integer> head;
+public class FastAndSlow {
+    Node1<Integer> head;
     int size;
 
     FastAndSlow() {
@@ -26,19 +23,53 @@ public class FastAndSlow {
         this.size = 0;
     }
 
-    @SuppressWarnings("unused")
-    public Node middleNode(Node head){
-        Node fast=head;
-        Node slow=head;
+    // returns the middle node (if even length -> returns second middle)
+    public Node1<Integer> middleNode(Node1<Integer> head) {
+        if (head == null) return null;
 
-        //what if  there is only one node then 
-        //that's why && operator
+        Node1<Integer> slow = head;
+        Node1<Integer> fast = head;
 
-        while(fast.next==null && fast==null){
-            fast=fast.next.next;
-            slow=slow.next;
+        // correct condition: check fast != null first, then fast.next != null
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
         return slow;
     }
 
+    // helper to append tail (for testing)  ....Inshort inserting in linked-list
+    public void append(int val) {
+        Node1<Integer> node = new Node1<>(val);
+        if (head == null) {
+            head = node;
+        } else {
+            Node1<Integer> t = head;
+            while (t.next != null) t = t.next;
+            t.next = node;
+        }
+        size++;
+    }
+
+    // test driver
+    public static void main(String[] args) {
+        FastAndSlow list = new FastAndSlow();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5); // odd length: 1,2,3,4,5
+
+        Node1<Integer> mid = list.middleNode(list.head);
+        System.out.println("Middle of 1..5 is: " + (mid != null ? mid.data : "null")); // 3
+
+        FastAndSlow list2 = new FastAndSlow();
+        list2.append(1);
+        list2.append(2);
+        list2.append(3);
+        list2.append(4); // even length: 1,2,3,4
+
+        Node1<Integer> mid2 = list2.middleNode(list2.head);
+        System.out.println("Middle of 1..4 is (second middle): " + (mid2 != null ? mid2.data : "null")); // 3
+    }
 }
